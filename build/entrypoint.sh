9c0a8f7e34a937cd3077c8d959ae8e71a34b54a8
Vladimir Ostapenco <vladost.pro@gmail.com>
2019-02-28 14:00:12 +0100

    New features

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
-echo "create wallet"
-
+# Writing wallet file
+echo "[INFO] Writing wallet from env variable to $IEXEC_WORKER_WALLET_PATH file."
 echo $IEXEC_WALLET_JSON > $IEXEC_WORKER_WALLET_PATH
 
+# Launching iExec Worker
+echo "[INFO] Launching iExec Worker"
 java -jar /iexec-worker.jar
